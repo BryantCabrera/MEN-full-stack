d024dfff32820588249586a89266e48868476c0b
Bryant <cabrera.bryant@gmail.com>
2019-01-19 14:29:25 -0800

    Adds server set up.

---
+++ server.js
@@ -0,0 +1,23 @@
+/********** REQUIRES **********/
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+
+
+/********** MIDDLEWARE **********/
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+
+/********** CONTROLLERS **********/
+
+
+/********** LISTENER **********/
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000.');
+});
